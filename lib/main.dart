6f1d011495af505d7ff00fd0094de54c5bbd42ab
João V. Sanches <joaovictorsanchesandradeestuda@gmail.com>
2024-12-24 01:24:34 -0300

    Enviando alterações

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:charlie_brown_jr/home/screen.dart';
+import 'package:charlie_brown_jr/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // Iniciando o aplicativo
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: Routes.initial_route,
+    routes: {Routes.home: (context) => Home()},
   ));
 }
---
+++ lib/routes.dart
@@ -0,0 +1,5 @@
+class Routes {
+  // Todas as rotas das screen se localiza aqui
+  static final String initial_route = home;
+  static final String home = '/home';
+}
